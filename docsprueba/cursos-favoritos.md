7343dbe2c5791f8f9d1bff69a98206b243d87220
eliza8855 <elopezvalerin@gmail.com>
2021-08-04 13:36:02 -0500

    creacion de primera funcion

---
+++ docsprueba/cursos-favoritos.md
@@ -0,0 +1,11 @@
+**Listado de Cursos Pendientes 2021**
+
+En este 2021 me he trazado la meta de llevar un curso nuevo por mes, a continuación se muestran todos los cursos proyectados para este año: 
+
+- Enero: [Curso de Photoshop 2021](https://www.crehana.com/clases/v2/10854/detalle/)
+- Febrero: [Introducción a la redacción digital](https://www.crehana.com/clases/v2/9325/player/21406/)
+- Marzo: [Nutrición saludable: Alimenta una vida mejor](https://www.crehana.com/hola/) 
+- Abril: [Participación ciudadana en el Parlamento Peruano](https://www.congreso.gob.pe/control-politico/index.php)
+- Mayo:  [Javascript Algorithms and Data Structures](https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/)
+
+ 
--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-module.exports = () => {
-  // ...
+const path = require("path");
+const fs = require("fs");
+
+const convertToPathAbsolute = (inputPath) => {
+  if (path.isAbsolute(inputPath)) {
+     return inputPath;
+  } else {
+     return path.resolve(inputPath);
+  }
 };
 
-console.log("hola")
+statsObj = fs.statSync("./docsprueba/cursos-favoritos.md");
+
+const isFilePath = (route) => {
+   const stats =
+};
+
+const checkTypeObject = (inputPath) => {
+   let arrayPathFiles = [ ];
+  if (fs.statSync(inputPath).isDirectory()) {
+    const readDirectory = fs.readdir(inputPath, (err,files) => {
+       if (err) {
+          return console.error(err);
+       } else {
+          
+       }
+
+    })
+    readDirectory.forEach(file => {
+       const pathFile = path.join(inputPath,file);
+       arrayPathFiles = arrayPathFiles.concat(checkTYpeObject(pathFile))
+    })
+     return arrayPathFiles 
+  } else {
+     return console.log("it is a file",convertToPathAbsolute(inputPath))  
+  }
+};
+
+console.log(statsObj);
+//console.log(checkTypeObject("./docsprueba/cursos-favoritos.md"));
+//console.log(convertToPathAbsolute("docsprueba"));
