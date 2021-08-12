c0ef7931e54ce425298c65abfbbe63cf9d1821a3
eliza8855 <elopezvalerin@gmail.com>
2021-08-12 16:02:50 -0500

    cambios version2

--- callback.js
+++ callback.js
@@ -1,15 +1,32 @@
-let p = new Promise(function(resolve, reject){
-        let sum = 2 + 6;
-        if (sum === 8){
-           resolve("correcto!");
-        }else {
-           reject("Error!");
-        }
-});
+/*const usersList = [
+    {id: 1, name: "Dominicode", city: 4},
+    {id: 2, name: "Wilfredo", city: 3},
+    {id: 3, name: "Alberto", city: 2},
+    {id: 4, name: "Dany", city: 1},
+];
 
-p.then(function(mensaje){
-    console.log(mensaje);
-})
-.catch(function(error){
-    console.log(error);
-});
+const citiesList = {
+    1: "Barcelona",
+    2: "New York",
+    3: "Santo Domingo",
+    4: "Madrid",
+};
+
+const getUsers = () => {
+    return new Promise((resolve,reject)=>{
+        setTimeout( ()=> {
+            resolve(usersList);
+        }, 2000)
+    });
+}
+
+getUsers().then( res => {
+    console.log("users", res);
+});*/
+
+let nombres = ["ELizabeth", "Mariella", "Jose", "Pedro", "Juan"];
+const object = { nombre: "Elizabeth", apellido: "Lopez", edad:31}
+
+for (const data in object) {
+    console.log (`${data}: ${object[data]}`);
+}
--- index.js
+++ index.js
@@ -3,19 +3,60 @@ const fs = require("fs");
 
 const absolutePathFile = "C:/Users/elope/OneDrive/Documents/GitHub/LIM015-md-links/docsprueba/cursos-favoritos.md";
 const relativePathFile = "./docsprueba/cursos-favoritos.md";
-const absolutePathDirectory = "C:\Users\elope\OneDrive\Documents/GitHub/LIM015-md-links/docsprueba";
+const absolutePathDirectory = "C:/Users/elope/OneDrive/Documents/GitHub/LIM015-md-links/docsprueba";
+const relativePathDirectory = "./docsprueba/";
 
 // Funcion que convierte la ruta a absoluta
 const convertPathToAbsolute = inputPath => path.resolve(inputPath);
 
-// Funcion que detecta si el file existe, devuelve un booleano 
+// Funcion que detecta si el file/directorio existe, devuelve un booleano 
 const detectPathExists = inputPath => fs.existsSync(inputPath);
 
-//Funcion que detecta si el directorio existe, devuelve un booleano
+//Funcion que detecta si la url es directorio, devuelve un booleano
+const detectDirectory = inputPath => fs.statSync(inputPath).isDirectory;
 
-const a = path.normalize(absolutePathDirectory);
-console.log(a);
+//Funcion que abre y muestra archivos de un directorioc
 
-//console.log(detectPathExists(absolutePathFile));
-//console.log(checkTypeObject("./docsprueba"));
-//console.log(path);
+
+const openDirectory = (inputPath) => {
+        let listFiles = fs.readdirSync(inputPath);
+        let filesArray = [];
+        listFiles.forEach( (file) => {
+        const pathChild = path.resolve(inputPath,file)
+          if (fs.statSync(pathChild).isFile()){
+              filesArray.push(pathChild);
+          } else {
+               const newDirectory = openDirectory(pathChild);
+               filesArray = filesArray.concat(newDirectory);
+          }
+        })
+        return filesArray;
+    };
+
+console.log(openDirectory(absolutePathDirectory));
+
+//Funcion que filtra un array y devuelve un array con solo archivos .md 
+
+const filesArray2 = [
+    //'C:\\Users\\elope\\OneDrive\\Documents\\GitHub\\LIM015-md-links\\docsprueba\\cursos-favoritos.md',
+    //'C:\\Users\\elope\\OneDrive\\Documents\\GitHub\\LIM015-md-links\\docsprueba\\cursos-favoritos3.md',
+    'C:\\Users\\elope\\OneDrive\\Documents\\GitHub\\LIM015-md-links\\docsprueba\\docsprueba2',
+    'C:\\Users\\elope\\OneDrive\\Documents\\GitHub\\LIM015-md-links\\docsprueba\\docsprueba2.js'
+  ]
+
+const FilterMdFile = (inputPath) => {
+    const listFilesMd = inputPath.filter(file => path.extname(file) == ".md");
+    if (listFilesMd.length === 0) {
+       return "There isn't any Markdown Files";
+    } else {
+       return listFilesMd;
+    }
+};
+
+// funcion que extrae las URL del los archivos
+
+const 
+    
+//console.log(FilterMdFile(filesArray))
+
+// 
